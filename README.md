93740482d7d6ef1b6466a825978600be458f6f63
nspektor <nellie.spektor@gmail.com>
2020-10-01 14:55:23 -0400

    Update validator and readme to work with problem specifications

---
+++ README.md
@@ -0,0 +1,33 @@
+# Ambulance Pickup Problem Architecture
+### Team Marshmallow (Nellie Spektor and Nehemiah Dureus)
+ns104@nyu.edu
+
+### Problem Statement (taken from the course website, just here as a referesher):
+- In our case, the graph is the Manhattan grid with every street going both ways. 
+- It takes a minute to go one block either north-south or east-west. 
+- Each hospital has an (x,y) location that you can determine when you see the distribution of victims. 
+- The ambulances need not return to the hospital where they begin. 
+- Each ambulance can carry up to 4 people. 
+- It takes 1 minute to load a person and 1 minute to unload up to four people. 
+- Each person will have a rescue time which is the number of minutes from now when the person should be unloaded in the hospital to survive. 
+[link to course website](https://cs.nyu.edu/courses/fall20/CSCI-GA.2965-001/notipping.html)
+
+## How do I interact with the architecture?
+Your file should output a result file that follows the format shown below. Please look at the `sample_result` files in this repo for some examples.
+```
+Hospital: <x>, <y>, <num_ambulances> 
+Hospital: <x>, <y>, <num_ambulances> 
+
+Ambulance: <hospital_id>: (<hospital_x>,<hospital_y>) <person_id>: (<person_x>,<person_y>,<person_rescue_time>), <person_id>: (<person_x>,<person_y>,<person_rescue_time>), <person_id>: (<person_x>,<person_y>,<person_rescue_time>)
+Ambulance: <hospital_id>: (<hospital_x>,<hospital_y>) <person_id>: (<person_x>,<person_y>,<person_rescue_time>), <person_id>: (<person_x>,<person_y>,<person_rescue_time>), <person_id>: (<person_x>,<person_y>,<person_rescue_time>), <person_id>: (<person_x>,<person_y>,<person_rescue_time>)
+Ambulance: <hospital_id>: (<hospital_x>,<hospital_y>) <person_id>: (<person_x>,<person_y>,<person_rescue_time>), <person_id>: (<person_x>,<person_y>,<person_rescue_time>)
+```
+
+To run the validator on your own, you can run it with python3:
+`python3 validator.py sample_data sample_result`
+Running this line should result in a total score of 26 and output showing who was rescued. 
+You can also try out running with `sample_wrong_result` to see the various kinds of error messages the validator produces.
+
+## How do I send in the code?
+Please send me (ns104@nyu.edu) a zip file by 12pm on Monday with your code and a `README.md` detailing how I should run/compile it. 
+Your solution should expect a command line argument with the name of the data file and should output a result file with your solution.
--- validator.py
+++ validator.py
@@ -1,6 +1,8 @@
 # !/usr/bin/env python
 
-import sys, os, re, fileinput
+import fileinput
+import re
+import sys
 
 
 # Exception classes
@@ -64,7 +66,7 @@ class Hospital:
         return
 
     def __repr__(self):
-        return '%d: (%d,%d)' % (self.hid, self.x, self.y)
+        return f'{self.hid}: ({self.x},{self.y})'
 
     def decrement_number_of_ambulances(self, t):
         self.ambulance_time[t] -= 1
@@ -78,54 +80,58 @@ class Hospital:
         self.ambulance_time[t] += 1
         return
 
-    def rescue(self, pers, hospitals):
-        if 4 < len(pers):
-            raise IllegalPlanError('Cannot rescue more than four people at once: %s' % pers)
-        already_rescued = [p for p in pers if p.rescued]
+    def rescue(self, people_on_ambulance, hospitals):
+        if 4 < len(people_on_ambulance):
+            raise IllegalPlanError('Cannot rescue more than four people at once: %s' % people_on_ambulance)
+        already_rescued = [p for p in people_on_ambulance if p.rescued]
         if already_rescued:
             raise IllegalPlanError('Person already rescued: %s' % already_rescued)
         # t: time to take
         # note: we don't have to go back to the starting hospital
-        # so go to the closest from the last student
+        # so go to the closest from the last person
         t = 0
-        start = self
-        for p in pers:
-            t += travel_time(start, p)
-            start = p
-        # look for closest hospital
-        min_hosp = 0
-        min_hosp_distance = None
+        curr_location = self
+        for p in people_on_ambulance:
+            t += travel_time(curr_location, p)  # add time for traveling to person
+            t += 1  # add 1 minute for loading the person
+            curr_location = p
+        # look for closest hospital to return to
+        closest_hospital = 0
+        travel_time_to_closest_hospital = None
         for hosp in hospitals:
-            tmp_dist = travel_time(start, hosp)
-            if not min_hosp_distance:
-                min_hosp_distance = tmp_dist
-                min_hosp = hosp
-            elif tmp_dist < min_hosp_distance:
-                min_hosp_distance = tmp_dist
-                min_hosp = hosp
-        t += min_hosp_distance
+            temp_dist = travel_time(curr_location, hosp)
+            if not travel_time_to_closest_hospital:
+                travel_time_to_closest_hospital = temp_dist
+                closest_hospital = hosp
+            elif temp_dist < travel_time_to_closest_hospital:
+                travel_time_to_closest_hospital = temp_dist
+                closest_hospital = hosp
+        t += travel_time_to_closest_hospital
+        t += 1  # add time for unloading up to 4 people
 
-        # try to schedule from the busiest ambulance at the hospital.
+        # check if there are any people on the ambulance that wont make it in time
         for (i, t0) in enumerate(self.ambulance_time):
-            if not [p for p in pers if p.rescue_time < t0 + t]: break
+            if not [p for p in people_on_ambulance if p.rescue_time < t0 + t]:
+                break
         else:
-            raise IllegalPlanError('Either person cannot make it: %s' % pers)
+            raise IllegalPlanError(f'Either of these people cannot make it: {people_on_ambulance}')
         # proceed the time.
         self.ambulance_time[i] += t
         # keep it sorted.
         self.ambulance_time.sort()
         self.ambulance_time.reverse()
-        for p in pers:
+        for p in people_on_ambulance:
             p.rescued = True
-        print('Rescued:', ' and '.join(map(str, pers)), 'taking', t, '|ended at hospital', min_hosp)
+        print('Rescued:', ' and '.join(map(str, people_on_ambulance)), 'taking time:', t,
+              ' with the ambulance ending at hospital', closest_hospital)
         return
 
 
-def read_data(fname):
+def read_input_data(fname):
     print('Reading data:', fname, file=sys.stderr)
     people = []
     hospitals = []
-    mode = 0
+    mode = 0  # 0: default, 1: people data, 2: hospital data
     with open(fname, 'r') as input_data:
         for line in input_data:
             line = line.strip().lower()
@@ -135,11 +141,11 @@ def read_data(fname):
                 mode = 2
             elif line:
                 if mode == 1:
-                    (a, b, c) = list(map(int, line.split(",")))
-                    people.append(Person(a, b, c))
+                    (x, y, t) = list(map(int, line.split(",")))
+                    people.append(Person(x, y, t))
                 elif mode == 2:
-                    (c,) = list(map(int, line.split(",")))
-                    hospitals.append(Hospital(-1, -1, c))
+                    (n,) = list(map(int, line.split(",")))
+                    hospitals.append(Hospital(-1, -1, n))
     return people, hospitals
 
 
@@ -150,70 +156,70 @@ def read_results(people, hospitals):
     p3 = re.compile(r'(\d+)\s*:\s*\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)')
 
     score = 0
-    mode = 0  # 0: default, 1: hospital locations, 2: ambulance responses
     hospital_index = 0
+    # reads from stdin by default, or from a result_file if specified in the command line args
     for line in fileinput.input():
         line = line.strip().lower()
-        if not line: continue
-        # check for hospital coordinates
+        if not line:
+            continue
+        # check hospital coordinates
         if line.startswith('hospital'):
-            mode = 1
             # read in hospital coordinates and set on hospital object
-            (x, y, z) = list(map(int, line.replace('hospital:', '').split(',')))
-            print("Hospital #{idx}: coordinates ({x},{y})".format(x=x, y=y, idx=hospital_index + 1))
+            (x, y, num_ambulances) = list(map(int, line.replace('hospital:', '').split(',')))
+            print(f"Hospital #{hospital_index + 1}: coordinates ({x},{y})")
             if not (x and y):
                 raise ValidationError("Hospital coordinates not set: line".format(line))
-            if z != hospitals[hospital_index].num_ambulances:
+            if num_ambulances != hospitals[hospital_index].num_ambulances:
                 raise ValidationError(
-                    "Hospital's ambulance # does not match input (input={0}, results={1})".format(
-                        hospitals[hospital_index].num_ambulances,
-                        z))
+                    f"Hospital's ambulance # does not match input "
+                    f"(input={hospitals[hospital_index].num_ambulances}, "
+                    f"results={num_ambulances})")
             hospitals[hospital_index].x = x
             hospitals[hospital_index].y = y
             hospital_index += 1
             continue
 
-        # check for ambulance records
+        # check ambulance movements
         if not line.startswith('ambulance'):
             print('!!! Ignored line: %r' % line, file=sys.stderr)
             continue
         try:
-            hos = None
-            rescue_persons = []
-            for (i, (w, z)) in enumerate(p1.findall(line)):
-                m = p2.match(w)
-                if m:
-                    # Hospital n:(x,y)
+            hospital = None
+            people_rescued = []
+            for (i, (w, num_ambulances)) in enumerate(p1.findall(line)):
+                hospital_match = p2.match(w)
+                if hospital_match:
+                    # Hospital hospital_id:(x,y)
                     if i != 0:
                         raise FormatSyntaxError('Specify a person now: %r' % line)
-                    (a, b, c) = list(map(int, m.groups()))
-                    if a <= 0 or len(hospitals) < a:
-                        raise FormatSyntaxError('Illegal hospital id: %d' % a)
-                    hos = hospitals[a - 1]
-                    if hos.x != b or hos.y != c:
-                        raise DataMismatchError('Hospital mismatch: %s != %d:%s' % (hos, a, (b, c)))
+                    (hospital_id, x, y) = list(map(int, hospital_match.groups()))
+                    if hospital_id <= 0 or hospital_id > len(hospitals):
+                        raise FormatSyntaxError(f'Illegal hospital id: {hospital_id}')
+                    hospital = hospitals[hospital_id - 1]
+                    if hospital.x != x or hospital.y != y:
+                        raise DataMismatchError(f'Hospital location mismatch: {hospital} != {hospital_id}: ({x},{y})')
                     continue
-                m = p3.match(w)
-                if m:
+                person_match = p3.match(w)
+                if person_match:
                     # Person n:(x,y,t)
                     if i == 0:
                         raise FormatSyntaxError('Specify a hospital first: %r' % line)
-                    (a, b, c, d) = list(map(int, m.groups()))
-                    if a <= 0 or len(people) < a:
-                        raise FormatSyntaxError('Illegal person id: %d' % a)
-                    per = people[a - 1]
-                    if per.x != b or per.y != c or per.rescue_time != d:
-                        raise DataMismatchError('Person mismatch: %s != %d:%s' % (per, a, (b, c, d)))
-                    rescue_persons.append(per)
+                    (person_id, x, y, rescue_time) = list(map(int, person_match.groups()))
+                    if person_id <= 0 or person_id > len(people):
+                        raise FormatSyntaxError(f'Illegal person id: {person_id}')
+                    person = people[person_id - 1]
+                    if person.x != x or person.y != y or person.rescue_time != rescue_time:
+                        raise DataMismatchError(f'Person mismatch: {person} != {person_id}: ({x, y, rescue_time})')
+                    people_rescued.append(person)
                     continue
                 # error
                 raise FormatSyntaxError('Expected "n:(x,y)" or "n:(x,y,t)": %r' % line)
 
-            if not hos or not rescue_persons:
+            if not hospital or not people_rescued:
                 print('!!! Insufficient data: %r' % line, file=sys.stderr)
                 continue
-            hos.rescue(rescue_persons, hospitals)
-            score += len(rescue_persons)
+            hospital.rescue(people_rescued, hospitals)
+            score += len(people_rescued)
         except ValidationError as x:
             print('!!!', x, file=sys.stderr)
     print('Total score:', score)
@@ -222,8 +228,8 @@ def read_results(people, hospitals):
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print('usage: validator.py datafile [resultfile]')
+        print('usage: validator.py datafile resultfile') #resultfile is optional
         sys.exit(2)
-    people_data, hospital_data = read_data(sys.argv[1])
+    people_data, hospital_data = read_input_data(sys.argv[1])
     del sys.argv[1]
     read_results(people_data, hospital_data)
